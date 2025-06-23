721ce53775092f2c6c558f0ad2b9bef74e27951a
Emerald <eugenia.polishchuk@gmail.com>
2025-06-23 12:19:12 +0300

    project renamed

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'gh-pages' ? '/browser-extensions-manager-ui-main/' : '/',
+  base: mode === 'gh-pages' ? '/browser-extensions-manager-ui/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
